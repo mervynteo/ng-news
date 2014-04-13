d099a384a3c86b0a2da6670c9887f0446ca3246d
Benjamin Fradet <benjamin.fradet@gmail.com>
2014-04-13 15:33:50 +0200

    ang-news

---
+++ app/scripts/app.js
@@ -0,0 +1,28 @@
+'use strict';
+
+/*global app: true */
+
+var app = angular.module('angNewsApp', [
+  'ngCookies',
+  'ngResource',
+  'ngSanitize',
+  'ngRoute',
+  'firebase'
+])
+  .constant('FIREBASE_URL',
+    'https://resplendent-fire-7525.firebaseio.com/');
+
+app.config(function ($routeProvider) {
+  $routeProvider
+    .when('/', {
+      templateUrl: 'views/posts.html',
+      controller: 'PostsCtrl'
+    })
+    .when('/posts/:postId', {
+      templateUrl: 'views/showpost.html',
+      controller: 'PostViewCtrl'
+    })
+    .otherwise({
+      redirectTo: '/'
+    });
+});
---
+++ app/scripts/controllers/posts.js
@@ -0,0 +1,9 @@
+'use strict';
+
+app.controller('PostsCtrl', function($scope, $location, Post) {
+  $scope.posts = Post.all;
+
+  $scope.deletePost = function(postId) {
+    Post.delete(postId);
+  };
+});
